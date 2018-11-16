f3be0fcd4027ce93f67a3f3878779df68ed4bf25
Antoine07 <antoine.lucsko@gmail.com>
2018-11-16 15:08:46 +0100

    timer correction home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs'; // Observable
+import { take, map } from 'rxjs/operators'; // opérateurs
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'app-music';
+  time : string;
 
-  prepareRoute(outlet: RouterOutlet) {
-    return  ;
-  }
+  constructor(){
+    
+    // interval envoi toutes les secondes un compteur 1, 2, ...
+    const interval$ = interval(1000).
+      pipe(
+        map(second => {
+          let hours = Math.floor(second / 3600);
+          let minutes = Math.floor(second / 60);
+
+          return `${hours} h ${minutes - hours * 60} min ${second - minutes * 60} s`
+        }),
+        take(12 * 60 * 3) // permet d'arrêter ici au bout de 12*3 minutes interval particulier à interval RxJS 6
+      )
+
+    // on souscrit à l'Observable interval
+    interval$.subscribe(
+      time => this.time = time
+    );
+  } 
 }
