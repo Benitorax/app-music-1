af911009a7b500d0f557ddefaeef0f02707893b5
Antoine07 <antoine.lucsko@gmail.com>
2018-12-01 09:40:00 +0100

    deleted album

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,10 +6,12 @@ import { AddAlbumComponent } from './add-album/add-album.component';
 import { Routes, RouterModule } from '@angular/router';
 import { GuardService } from '../guard.service';
 import { UpdateAlbumComponent } from './update-album/update-album.component';
+import { DeleteAlbumComponent } from './delete-album/delete-album.component';
 
 const routes: Routes = [
   { path: 'admin/add', /*canActivate: [GuardService],*/ component: AddAlbumComponent },
   { path: 'admin/update/:id', /*canActivate: [GuardService],*/ component: UpdateAlbumComponent },
+  { path: 'admin/delete/:id/:action',/*canActivate: [GuardService],*/ component: DeleteAlbumComponent },
 ]
 
 @NgModule({
@@ -18,7 +20,7 @@ const routes: Routes = [
     ShareModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [AlbumComponent, AddAlbumComponent, UpdateAlbumComponent],
+  declarations: [AlbumComponent, AddAlbumComponent, UpdateAlbumComponent, DeleteAlbumComponent],
   exports : [AlbumComponent]
 })
 export class AdminModule { }
