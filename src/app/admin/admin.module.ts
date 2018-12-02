3e127e268f916b29908ba43b89ecf24e683d0d8d
Antoine07 <antoine.lucsko@gmail.com>
2018-12-02 11:16:02 +0100

    modal chapEnd

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { GuardService } from '../guard.service';
 import { UpdateAlbumComponent } from './update-album/update-album.component';
 import { DeleteAlbumComponent } from './delete-album/delete-album.component';
+import { ModalDialogComponent } from './modal-dialog/modal-dialog.component';
 
 const routes: Routes = [
   { path: 'admin/add', /*canActivate: [GuardService],*/ component: AddAlbumComponent },
@@ -20,7 +21,7 @@ const routes: Routes = [
     ShareModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [AlbumComponent, AddAlbumComponent, UpdateAlbumComponent, DeleteAlbumComponent],
+  declarations: [AlbumComponent, AddAlbumComponent, UpdateAlbumComponent, DeleteAlbumComponent, ModalDialogComponent],
   exports : [AlbumComponent]
 })
 export class AdminModule { }
